9acfbb5f3b3b5bdeea11c493e5897873e521023b
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-11-26 20:26:32 +0330

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
